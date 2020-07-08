df92b4d7b7ca312f902907bf69cda89ac697e598
gabsguida <gabrielaguida7@gmail.com>
2020-07-08 20:21:22 -0300

    main vue instance

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue';
+
 import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import pinDirective from './shared/pin-directive';
+import currencyFilters from './shared/currency-filter';
 
 Vue.config.productionTip = false;
 
+// declaring directives globally -> Vue.directive(pinName, source)
+Vue.directive('pin', pinDirective);
+
+// declaring filters globally -> Vue.filter(filterName, source)
+Vue.filter('currency', currencyFilters);
+
 new Vue({
   render: (h) => h(App),
+  router,
+  store,
 }).$mount('#app');
